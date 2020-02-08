4cdea1e90f7e88785e53039a7528ff61f0584394
frolkin28 <frolkin2801@gmail.com>
2020-02-08 22:08:01 +0200

    solution changed

--- alg1.py
+++ alg1.py
@@ -1,32 +1,34 @@
 import numpy as np
+import re
 
 
-def take_norm_vector(flat):
-	x = flat.index('x')
-	y = flat.index('y')
-	z = flat.index('z')
-	norm_vector = list(map(float, [flat[:x], flat[x + 1: y], flat[y + 1: z]]))
+def take_norm_vector(data):
+	pat = r'([+-]?\d*)x([+-]?\d*)y([+-]?\d*)z[+-]?\d*=0'
+	flat = re.search(pat, data)
+	if not flat:
+		raise ValueError('Incorrect input, check equation')
+	norm_vector = []
+	# get coordinates of normal vector from regex
+	for i in flat.groups():
+		if i == '+' or i == '':
+			norm_vector.append(1.0)
+		elif i == '-':
+			norm_vector.append(-1.0)
+		else:
+			norm_vector.append(float(i))
 	return norm_vector
 
 
 def solve(flat1, flat2, dot):
+	# get two normal vectors
 	n1 = take_norm_vector(flat1)
 	n2 = take_norm_vector(flat2)
+	# multiply them to get normal vector for our flat
 	norm_vector = list(np.cross(n1, n2))
 	result = '{}(x - {}) + {}(y - {}) + {}(z - {}) = 0'.format(
 		norm_vector[0], dot[0], norm_vector[1], dot[1], norm_vector[2], dot[2])
+	# make result looks better
 	result = result.replace('- -', '+ ')
 	result = result.replace('+ -', '- ')
 	return result
 
-
-def main():
-	flat1 = '4x+1y-3z+13=0'
-	flat2 = '1x-2y+1z-11=0'
-	dot = [-3, 2, 5]
-	result = solve(flat1, flat2, dot)
-	print(result)
-
-
-if __name__ == '__main__':
-	main()
--- test_alg1.py
+++ test_alg1.py
@@ -4,8 +4,8 @@ from alg1 import solve
 
 class Test(unittest.TestCase):
 	def test_solution(self):
-		flat1 = '4x+1y-3z+13=0'
-		flat2 = '1x-2y+1z-11=0'
+		flat1 = '4x+y-3z+13=0'
+		flat2 = 'x-2y+z-11=0'
 		dot = [-3, 2, 5]
 		result = solve(flat1, flat2, dot)
 		self.assertEqual(result, '-5.0(x + 3) - 7.0(y - 2) - 9.0(z - 5) = 0')
